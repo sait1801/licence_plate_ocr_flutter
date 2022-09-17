4d8193198d806739d3afd5e3548cd35272a55ebf
YusufOnatYilmaz <yusufonatyilmaz@gmail.com>
2022-09-17 15:58:31 +0300

    sait

--- lib/src/plate_detector.dart
+++ lib/src/plate_detector.dart
@@ -57,45 +57,32 @@ class _TextRecognizerViewState extends State<TextRecognizerView> {
     setState(() {
       _text = '';
     });
-    final recognizedText = await _textRecognizer.processImage(inputImage);
-    List<String> recognisedarray = recognizedText.text.split("\n");
-    for (var arr in recognisedarray) {
-      arr = arr.replaceAll(" ", "");
-      if (arr.length == 7) {
-        String num_part = arr.substring(0, 4);
-        String str_part = arr.substring(4);
-        if (_isNumeric(num_part)) {
-          if (!(_isNumeric(str_part[0])) &&
-              !(_isNumeric(str_part[1])) &&
-              !(_isNumeric(str_part[2]))) {
-            print("**********************************");
-            print(arr);
-            print("**********************************");
-
-            if (await FireStoreHelper().checkPlates(recognizedText.text)) {
-              FireStoreHelper().createPlate(recognizedText.text);
-            } else {
-              print("Plate Already Exist");
-            }
-          }
-        }
-        ;
-      }
-    }
-
     if (inputImage.inputImageData?.size != null &&
         inputImage.inputImageData?.imageRotation != null) {
-      final painter = TextRecognizerPainter(
-          recognizedText,
-          inputImage.inputImageData!.size,
-          inputImage.inputImageData!.imageRotation);
-      _customPaint = CustomPaint(painter: painter);
-    } else {
-      _text = 'Recognized text:\n\n${recognizedText.text}';
-      // TODO: set _customPaint to draw boundingRect on top of image
-      _customPaint = null;
-      if (_text != null) {
-        print("here");
+      final recognizedText = await _textRecognizer.processImage(inputImage);
+      List<String> recognisedarray = recognizedText.text.split("\n");
+      for (var arr in recognisedarray) {
+        arr = arr.replaceAll(" ", "");
+        if (arr.length == 7) {
+          String num_part = arr.substring(0, 4);
+          String str_part = arr.substring(4);
+          if (_isNumeric(num_part)) {
+            if (!(_isNumeric(str_part[0])) &&
+                !(_isNumeric(str_part[1])) &&
+                !(_isNumeric(str_part[2]))) {
+              print("**********************************");
+              print(arr);
+              print("**********************************");
+
+              if (await FireStoreHelper().checkPlates(recognizedText.text)) {
+                FireStoreHelper().createPlate(recognizedText.text);
+              } else {
+                print("Plate Already Exist");
+              }
+            }
+          }
+          ;
+        }
       }
     }
     _isBusy = false;
